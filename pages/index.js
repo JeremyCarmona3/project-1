1df104199ae572509962af475c2fc6640ea155c5
bachvanquangmc <bachvanquangmc@gmail.com>
2022-01-13 20:32:31 -0800

    index changes

--- pages/index.js
+++ pages/index.js
@@ -11,19 +11,42 @@ import PlusButton from '../comps/buttons/plusButton';
 
 import { useRouter } from 'next/router'
 
+import React, {useState} from 'react';
+
 export default function Home() {
   const router = useRouter()
+  const foods = [
+    { id:0,
+      name: "Apple",
+      date: "20/12/2022",
+    },
+    { id:1,
+      name: "Orange",
+      date: "10/1/2022",
+    }
+  ]
 
+  const [value, setValue] = useState(null)
+  const inputValue = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.appCont}>
         <div className={styles.flex}>
           <HeaderCard />
-          <InputCard onClick={() => router.push('/infopage')} />
+          {foods.map(f =>(
+              <InputCard foodItem={f.name} onClick={() => router.push('/infopage')} />
+          ))}
+         
         </div>
         <div className={styles.flex}>
           <HeaderCard />
-          <ExpCard />
+
+          {foods.map(f =>(
+            <ExpCard headerText={f.name} expiryDate={f.date} />
+
+          ))}
           <PlusButton />
         </div>
         {/* <Button />
